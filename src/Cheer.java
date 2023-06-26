02bc7c3f7b5c29ac642d4709e884302481436753
Václav <nasinec.v@seznam.cz>
2023-06-26 14:41:54 +0200

    d6

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
     public static void main(String[] args) {
-        cheer("Fuckin DA Java ", 69);
+        cheer("Cheer for the ", 69);
     }
     public static void cheer(String name, int age){
         System.out.println(name + age);
