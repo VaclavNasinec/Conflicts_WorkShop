95e73b2c65c3a1350e6242842c7b195ea1fa9c93
Arina Siedina <orynasiedina@gmail.com>
2023-06-26 13:28:25 +0200

    a4

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
     public static void main(String[] args) {
-        cheer("Fuckin Java");
+        cheer("Kill Me");
     }
     public static void cheer(String name){
         System.out.println(name);
