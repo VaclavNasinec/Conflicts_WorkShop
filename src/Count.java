4596b45978c7eb1ff989bfdd68452e1c0588939a
Tomas Emr <tommy.emr@gmail.com>
2023-06-26 14:42:14 +0200

    c6

--- src/Count.java
+++ src/Count.java
@@ -1,8 +1,9 @@
 public class Count {
     public static void main(String[] args) {
-    count(33,"Karel");
+        count(33);
     }
-    public static void count(int age,String name){
-        System.out.println(age);
+
+    public static void count(int age) {
+        System.out.println("Counting the countless" + age);
     }
 }
