51298e74a95ce8ae8b3d97b82712dcefe654199f
Václav <nasinec.v@seznam.cz>
2023-06-26 13:13:08 +0200

    d3

--- src/Greet.java
+++ src/Greet.java
@@ -1,6 +1,6 @@
 public class Greet {
     public static void main(String[] args) {
-    greet("Arina");
+    greet("Greetings");
     }
     public static void greet(String jmeno){
         System.out.println(jmeno);
