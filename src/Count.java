c86d43531a8208dfee690beb96a703cb9a69f1ef
Cendos88 <cendos88@gmail.com>
2023-06-26 14:27:53 +0200

    b5

--- src/Count.java
+++ src/Count.java
@@ -1,8 +1,8 @@
 public class Count {
     public static void main(String[] args) {
-    count(22);
+    count(33,"Karel");
     }
-    public static void count(int age){
+    public static void count(int age,String name){
         System.out.println(age);
     }
 }
